77c4b3ec3f38be4799cef0bf454001c605caec77
Felix <javazorb@pixelinvaderz.com>
2024-01-10 00:08:17 +0100

    minor change

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 import dataset.generate_environment as generate_data
-import dataset.dataset
+import dataset.dataset as data
 
 
 def run():
     generate_data.generate_and_save_environments(num_environments=1000)
-    training_data, validation_data, testing_data = dataset.dataset.train_test_val_split(generate_data.load_environments())
+    training_data, validation_data, testing_data = data.train_test_val_split(generate_data.load_environments())
 
 
 if __name__ == '__main__':
