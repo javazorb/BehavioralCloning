4d0c745cafbc4e6b9e05ae027be7e37308fc2243
Felix <javazorb@pixelinvaderz.com>
2024-04-03 00:03:11 +0200

    gitignore addition

--- run.py
+++ run.py
@@ -6,10 +6,11 @@ import json
 
 
 def run():
-    #generate_data.generate_and_save_environments(num_environments=1000)
-    #training_data, validation_data, testing_data = data.train_test_val_split(generate_data.load_environments())
+    # generate_data.generate_and_save_environments(num_environments=1000)
+    # training_data, validation_data, testing_data = data.train_test_val_split(generate_data.load_environments())
     envs = generate_data.load_environments()
-    save_optimal_paths(envs)
+    # save_optimal_paths(envs)
+    train_data, test_data, val_data = data.train_test_val_split()
 
 
 def save_optimal_paths(envs):
