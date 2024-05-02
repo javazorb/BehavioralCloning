e0d8194c6172747581c3be305a8074b2a50b3405
javazorb <javazorb@gmail.com>
2024-05-02 15:07:36 +0200

    added a first version of a CNN

--- model/model.py
+++ model/model.py
@@ -1,6 +1,26 @@
 import torch
+from torch import nn
+
 import config
 
+
+class BehavioralModelCNN(nn.Module):
+    def __init__(self):
+        super(BehavioralModelCNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, stride=1, padding=1)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(in_features=config.BATCH_SIZE*config.ENV_SIZE*config.ENV_SIZE, out_features=128)
+        self.fc2 = nn.Linear(in_features=128, out_features=240) # 60*4 = 240, 4 action probs per position
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = x.view(-1, config.BATCH_SIZE * config.ENV_SIZE * config.ENV_SIZE)
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        x = x.view(-1, 60, 4)
+        return x
 
 class BehavioralCloning(torch.nn.Module): # TODO going on with CNN
 
