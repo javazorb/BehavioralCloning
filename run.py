981bf1475288f1d9e27ad1c913d788af9f455574
Felix <javazorb@pixelinvaderz.com>
2024-06-27 00:43:08 +0200

    added todo

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ import torch.nn as nn
 import torch.optim as optim
 import environments.QEnvironment as QEnvironment
 
-
+# TODO add setup folder structure
 def run():
     # generate_data.generate_and_save_environments(num_environments=1000)
     # training_data, validation_data, testing_data = data.train_test_val_split(generate_data.load_environments())
